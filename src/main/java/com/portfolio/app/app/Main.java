995c202b76eaf75bae6d8662f530dd9615ac7c10
Charishma <charishmagadepalli@gmail.com>
2026-09-09 13:47:04 +0530

    first commit

---
+++ src/main/java/com/portfolio/app/app/Main.java
@@ -0,0 +1,15 @@
+package com.portfolio.app.app
+
+public class Main {
+
+	public static void main(String[] args) {
+	 System.out.println("===================");
+	 System.out.println("Portfolio Management Project");
+	 System.out.println("======================");
+	 User user=new User("101","Rishi","Rishi@gmail.com");
+	 System.out.println("User ID:",+ user.getUserid());
+	 System.out.println("Name:",+ user.getName());
+	 System.out.println("Email:",+ user.getEmail());
+	}
+
+}
